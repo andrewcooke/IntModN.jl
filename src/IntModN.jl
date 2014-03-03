c7b469ea16630e96160d0665d6b4c275100ab179
andrew cooke <andrew@acooke.org>
2014-03-02 21:16:26 -0300

    before renamingpoly

--- src/IntModN.jl
+++ src/IntModN.jl
@@ -47,11 +47,13 @@ end
 # but typically you would use one of these constructors
 
 ZR{I<:Integer}(n::Int, i, ::Type{I}) = ZRing{n,I}(validate(n, convert(I, i)))
-ZR(n::Int, i::Integer) = ZRing{n, typeof(i)}(validate(n, i))
+ZR(n, i::Integer) = ZRing{n, typeof(i)}(validate(n, i))
+ZR(n) = i::Integer -> ZR(n, i)  # used to construct constructors
 ZF{I<:Integer}(n::Int, i, ::Type{I}) = ZField{n,I}(validate(n, convert(I, i)))
-ZF(n::Int, i::Integer) = ZField{n, typeof(i)}(validate(n, i))
-Z2(n) = ZF(2, n)
-GF2 = Z2
+ZF(n, i::Integer) = ZField{n, typeof(i)}(validate(n, i))
+ZF(n) = i::Integer -> ZF(n, i)  # used to construct constructors
+
+GF2 = ZF(2)
 
 # and all the methods that make this a number
 
@@ -68,6 +70,7 @@ end
 
 convert{X<:Integer}(::Type{X}, z::ZRing) = convert(X, z.i)
 convert{X<:Integer}(::Type{X}, z::ZField) = convert(X, z.i)
+modulus{N, I}(::Type{Z{N, I}}) = N
 modulus{N, I}(::ZRing{N, I}) = N
 modulus{N, I}(::ZField{N, I}) = N
 
@@ -146,7 +149,8 @@ function test_z_constructor()
     @assert string(ZR(3, 2)) == "2 mod 3"
     @assert string(ZF(3, 2, Int)) == "2 mod 3"
     @assert string(ZF(3, 2)) == "2 mod 3"
-
+    
+    Z2 = ZF(2)
     @assert GF2(1) == Z2(1) == ZField{2,Int}(1) == ZF(2, 1)
 
     @assert isa(ZR(4, 0x3).i, Uint8)
@@ -183,25 +187,25 @@ end
 
 function test_z_matrix_inverse()
 
-    @zfield 2 begin
-        A = [1 1;
-             0 1]
-        b = [1, 1]
-        x = A\b
-        @assert x == [0, 1]
-    end
-
-    # http://math.stackexchange.com/questions/169921/how-to-solve-system-of-linear-equations-of-xor-operation
-    @zfield 2 begin
-        A = [1 1 1 0; 
-             1 1 0 1;
-             1 0 1 1;
-             0 1 1 1]
-        b = [1, 1, 0, 1]
-        x = A\b
-        @assert x == [0, 1, 0, 0]
-    end
-
+#    @zfield 2 begin
+#        A = [1 1;
+#             0 1]
+#        b = [1, 1]
+#        x = A\b
+#        @assert x == [0, 1]
+#    end
+#
+#    # http://math.stackexchange.com/questions/169921/how-to-solve-system-of-li#near-equations-of-xor-operation
+#    @zfield 2 begin
+#        A = [1 1 1 0; 
+#             1 1 0 1;
+#             1 0 1 1;
+#             0 1 1 1]
+#        b = [1, 1, 0, 1]
+#        x = A\b
+#        @assert x == [0, 1, 0, 0]
+#    end
+#
     println("test_z_matrix_inverse ok")
 end
 
@@ -218,16 +222,16 @@ function test_z_power()
 end
 
 function test_z_macros()
-    begin
-        b = 7
-    end
-    @assert b == 7 
-    @zfield 5  begin
-        b = 1 + 2 * 3
-        a = b / 3
-    end
-    @assert a == ZF(5, 4)
-    @assert ZR(3, 2) == @zring 3 1 + 4
+#    begin
+#        b = 7
+#    end
+#    @assert b == 7 
+#    @zfield 5  begin
+#        b = 1 + 2 * 3
+#        a = b / 3
+#    end
+#    @assert a == ZF(5, 4)
+#    @assert ZR(3, 2) == @zring 3 1 + 4
     println("test_z_macros ok")
 end
 
@@ -249,68 +253,71 @@ end
 
 # --- polynomials over integers modulo n
 
-abstract P{N, T<:Z}
-
-immutable PRing{N, T<:Z} <:P{N, T}    
+immutable P{T<:Z}
     a::Array{T,1}
-    function PRing(a)
-        @assert length(a) == N "wrong length ($N != length($a))"
-        @assert N > 0 "empty polynomial"
-        new(a)
-    end
+    P(a) = length(a) == 0 || a[length(a)] != 0 ? new(a) : error("zero leading coeff")
 end
 
 # constructors
 
-PR{T<:Z}(a::T...) = PRing{length(a), T}([a...])
-function PGF2(n::Int, indices::Int...)  # indices are 1-indexed
-    a, o = zeros(ZField{2,Int}, n), GF2(1)
-    for i in indices
-        a[i] = o
+function Poly{I<:Integer}(c::Function, coeffs::(I, Int)...)
+    coeffs = collect(filter(c -> c[2] > 0, coeffs))
+    m = maximum([i for (i, n) in coeffs])
+    t = typeof(c(zero(I)))
+    a = zeros(t, m)
+    for (i, n) in coeffs
+        a[i] = c(n)
     end
-    PR(a...)
+    P{t}(a)
+end
+
+function Poly{I<:Integer}(c::Function, coeffs::(I, Int)...)
 end
 
 # number methods
 
-zero{N,T<:Z}(::Type{PRing{N,T}}) = PRing{N,T}(zeros(T, N))
-one{N,T<:Z}(::Type{PRing{N,T}}) = PRing{N,T}(ones(T, N))  # maybe 0...01?
+zero{T<:Z}(::Type{P{T}}) = P(T[])
+one{T<:Z}(::Type{P{T}}) = P([one(T)])
 
 # does not create a new copy
-convert{N,T<:Z}(::Type{Array{T,1}}, a::PRing{N,T}) = a.a
+convert{T<:Z}(::Type{Array{T,1}}, a::P{T}) = a.a
 
-showcompact{N,T<:Z}(io::IO, p::PRing{N,T}) = showcompact(io, convert(Array{T,1}, p))
-function show{N,T<:Z}(io::IO, p::PRing{N,T})
-    for i in N:-1:1
-        if i < N
-            print(io, " + ")
-        end
-        showcompact(io, p.a[i])
-        if i == 2
-            print(io, " x")
-        elseif i > 2
-            print(io, " x^$(i-1)")
+showcompact{T<:Z}(io::IO, p::P{T}) = showcompact(io, convert(Array{T,1}, p))
+
+function show{T<:Z}(io::IO, p::P{T})
+    n = length(p.a)
+    if n == 0
+       print(io, "0")
+    else
+        for i in n:-1:1
+            if i < n
+                print(io, " + ")
+            end
+            showcompact(io, p.a[i])
+            if i == 2
+                print(io, " x")
+            elseif i > 2
+                print(io, " x^$(i-1)")
+            end
         end
     end
-    print(io, " mod $(modulus(p.a[1]))")  # array length > 0
+    print(io, " mod $(modulus(T))")
 end
 
-=={N,T<:Z}(a::P{N,T}, b::P{N,T}) = a.a == b.a
-<={N,T<:Z}(a::P{N,T}, b::P{N,T}) = a.a <= b.a
-<{N,T<:Z}(a::P{N,T}, b::P{N,T}) = a.a < b.a
+=={T<:Z}(a::P{T}, b::P{T}) = a.a == b.a
+<={T<:Z}(a::P{T}, b::P{T}) = a.a <= b.a
+<{T<:Z}(a::P{T}, b::P{T}) = a.a < b.a
 
 liftp(c, f, a) = c(f(a.a))
 liftp(c, f, a, b) = c(f(a.a, b.a))
--{N,T<:Z}(a::PRing{N,T}) = liftp(PRing{N,T}, -, a)
-+{N,T<:Z}(a::PRing{N,T}, b::PRing{N,T}) = liftp(PRing{N,T}, +, a, b)
--{N,T<:Z}(a::PRing{N,T}, b::PRing{N,T}) = liftp(PRing{N,T}, -, a, b)
-#*{N,T<:Z}(a::PRing{N,T}, b::PRing{N,T}) = liftp(PRing{N,T}, *, a, b)
-#^{N,T<:Z}(a::PRing{N,T}, p::Integer) = liftp(PRing{N,T}, x -> x^p, a)
+-{T<:Z}(a::P{T}) = liftp(P{T}, -, a)
++{T<:Z}(a::P{T}, b::P{T}) = liftp(P{T}, +, a, b)
+-{T<:Z}(a::P{T}, b::P{T}) = liftp(P{T}, -, a, b)
 
 
 function test_p_constructor()
-    @assert PRing{2,ZField{2,Int}}([GF2(0), GF2(1)]) == PR(GF2(0), GF2(1)) == PGF2(2, 2)
-    @assert PR(GF2(0), GF2(0)) == zero(PRing{2, ZField{2,Int}})
+    @assert P{ZField{2,Int}}([GF2(0), GF2(1)]) == Poly(ZF(2), (2, 1))
+    @assert Poly(GF2(0), GF2(0)) == zero(PRing{2, ZField{2,Int}})
     println("test_p_constructor ok")
 end
 
