6c714913a9ed94d702d25074b47a954edd82ad1a
andrew cooke <andrew@acooke.org>
2015-08-05 17:14:49 -0300

    add explicit type for modulus (needed for frozen values)

--- src/IntModN.jl
+++ src/IntModN.jl
@@ -1,4 +1,5 @@
 
+
 # TODO - overflow
 # TODO - fast mult as http://maths-people.anu.edu.au/~brent/pd/rpb232.pdf
 
@@ -22,8 +23,10 @@ module IntModN
 import Base: show, showcompact, zero, one, inv, real, abs, convert,
        promote_rule, length, getindex, setindex!, start, done, next,
        rand, rand!, print, map, leading_zeros, divrem, endof, bits,
-       div, rem, mod
+       div, rem, mod, trailing_zeros
 import Base.Random.AbstractRNG
+
+using AutoTypeParameters 
 
 export ZModN, ZField, ZRing, ZR, ZF, GF2, @zring, @zfield, 
        ZPoly, ZP, X, P, order, modulus, factor, itype,
@@ -54,8 +57,9 @@ inv{M<:Residue}(m::M) = error("no inverse defined")
 
 # --- integers modulo n
 
+# J is the type of N.  this may seem superfluous, but N itself is frozen, so its type is not always available (see AutoTypeParameters)
 
-abstract ZModN{N, I<:Integer} <: Residue
+abstract ZModN{J<:Integer, N, I<:Integer} <: Residue
 
 # we need two types here because prime moduli have a faster inverse
 # (via euler's theorem) (note - we do NOT test for primality).
@@ -63,13 +67,15 @@ abstract ZModN{N, I<:Integer} <: Residue
 # you can construct these directly, but code assumes that the integer is
 # already reduced mod N, that capacities are ok, etc.
 
-immutable ZRing{N, I<:Integer} <: ZModN{N,I}
+immutable ZRing{J<:Integer, N, I<:Integer} <: ZModN{J,N,I}
     i::I
+    ZRing(i) = (@assert isa(i, J); new(i))
 end
 
 # this assumes N is prime and uses a faster inverse
-immutable ZField{N, I<:Integer} <: ZModN{N,I}
+immutable ZField{J<:Integer, N, I<:Integer} <: ZModN{J,N,I}
     i::I
+    ZField(i) = (@assert isa(i, J); new(i))
 end
 
 # but typically you would use one of these constructors (ZF or ZR)
@@ -81,28 +87,32 @@ function prepare_z{I<:Integer}(n::Int, i::I)
 end
 
 for (f, Z) in ((:ZR, :ZRing), (:ZF, :ZField))
-    @eval $f{I<:Integer}(n::Int, i, ::Type{I}) = $Z{n,I}(prepare_z(n, convert(I, i)))
-    @eval $f(n::Int, i::Integer) = $Z{n, typeof(i)}(prepare_z(n, i))
-    @eval $f(n::Integer, i::Integer) = $Z{n, typeof(i)}(mod(i, n))
+    @eval $f{J<:Integer, I<:Integer}(n, ::Type{J}, i, ::Type{I}) = $Z{J, freeze(convert(J, n)), I}(prepare_z(n, convert(I, i)))
+    @eval $f{I<:Integer}(n::Int, i, ::Type{I}) = $Z{typeof(n), freeze(n), I}(prepare_z(n, convert(I, i)))
+    @eval $f(n::Int, i::Integer) = $Z{typeof(n), freeze(n), typeof(i)}(prepare_z(n, i))
+    @eval $f(n::Integer, i::Integer) = $Z{typeof(n), freeze(n), typeof(i)}(mod(i, n))
     @eval $f(n) = i::Integer -> $f(n, i)  # used to construct constructors
 end
 GF2 = ZF(2)
 
-modulus{N, I<:Integer}(::Type{ZModN{N, I}}) = N
-modulus{T<:ZModN}(::Type{T}) = modulus(super(T))  # jameson type chain
+modulus{J<:Integer, N, I<:Integer}(::Type{ZModN{J, N, I}}) = convert(J, thaw(eval, N))
+modulus{T<:ZModN}(::Type{T}) = modulus(super(T))  # jameson type chaina
 modulus{T<:ZModN}(::T) = modulus(T)
 order{T<:ZModN}(::Type{T}) = modulus(T)
 order{T<:ZModN}(::T) = modulus(T)
-itype{N, I<:Integer}(::Type{ZModN{N, I}}) = I
+itype{J, N, I<:Integer}(::Type{ZModN{J, N, I}}) = I
 itype{T<:ZModN}(::Type{T}) = itype(super(T))  # jameson type chain
 itype{T<:ZModN}(::T) = itype(T)
+ntype{J, N, I<:Integer}(::Type{ZModN{J, N, I}}) = J
+ntype{T<:ZModN}(::Type{T}) = ntype(super(T))  # jameson type chain
+ntype{T<:ZModN}(::T) = ntype(T)
 
 for (f, Z) in ((:ZR, :ZRing), (:ZF, ZField))
     # julia's comversion is pretty damn dumb at times
-    @eval convert{N,I<:Integer}(::Type{$Z{N,I}}, a::$Z{N,I}) = a
+    @eval convert{J<:Integer,N,I<:Integer}(::Type{$Z{J,N,I}}, a::$Z{J,N,I}) = a
     # this promotion needed because linalg code has direct compariosn with 0
-    @eval promote_rule{N, I<:Integer}(::Type{$Z{N,I}}, ::Type{Int}) = $Z{N,I}
-    @eval convert{N,I<:Integer}(::Type{$Z{N,I}}, i::Integer) = $f(N, convert(I, i))
+    @eval promote_rule{J<:Integer, N, I<:Integer}(::Type{$Z{J,N,I}}, ::Type{Int}) = $Z{J,N,I}
+    @eval convert{J<:Integer,N,I<:Integer}(::Type{$Z{J,N,I}}, i::Integer) = $f(convert(J, N), convert(I, i))
 end
 # conversion to int, but no promotion
 for T in (:Int, :Uint)
@@ -112,33 +122,39 @@ end
 # all the methods that make these numbers
 
 for Z in (:ZRing, :ZField)
-    @eval zero{N,I}(::Type{$Z{N,I}}) = $Z{N,I}(zero(I))
-    @eval one{N,I}(::Type{$Z{N,I}}) = $Z{N,I}(one(I))
+    @eval zero{J,N,I}(::Type{$Z{J,N,I}}) = $Z{J,N,I}(zero(I))
+    @eval one{J,N,I}(::Type{$Z{J,N,I}}) = $Z{J,N,I}(one(I))
 end
 zero{Z<:ZModN}(z::Z) = zero(Z)
 one{Z<:ZModN}(z::Z) = one(Z)
 
-showcompact{N,I}(io::IO, z::ZModN{N,I}) = showcompact(io, z.i)
-print{N,I}(io::IO, z::ZModN{N,I}) = print(io, "$(z.i) mod $N")
-show{N,I<:Integer}(io::IO, z::ZField{N,I}) = print(io, "ZField{$N,$I}($(z.i))")
-show{N,I<:Integer}(io::IO, z::ZRing{N,I}) = print(io, "ZRing{$N,$I}($(z.i))")
+showcompact{J,N,I}(io::IO, z::ZModN{J,N,I}) = showcompact(io, z.i)
+print{J,N,I}(io::IO, z::ZModN{J,N,I}) = print(io, "$(z.i) mod $N")
+show{J,N,I<:Integer}(io::IO, z::ZField{J,N,I}) = print(io, "ZField{$J,$N,$I}($(z.i))")
+show{J,N,I<:Integer}(io::IO, z::ZRing{J,N,I}) = print(io, "ZRing{$J,$N,$I}($(z.i))")
 
-rand{N,I<:Integer}(T::Type{ZRing{N,I}}) = ZR(N, rand(one(I):convert(I,N)))
-rand{N,I<:Integer}(T::Type{ZField{N,I}}) = ZF(N, rand(one(I):convert(I,N)))
+rand_{J,N,I<:Integer}(T::Type{ZRing{J,N,I}}) = ZR(N, J, rand(one(I):convert(I,modulus(T))), I)
+rand_{J,N,I<:Integer}(T::Type{ZField{J,N,I}}) = ZF(N, J, rand(one(I):convert(I,modulus(T))), I)
 function rand!{T<:ZModN}(::Type{T}, a::AbstractArray)
     for i in 1:length(a)
-        a[i] = rand(T)
+        a[i] = rand_(T)
     end
     a
 end
+
 rand(::Type{ZModN}, dims...) = error("use more specific type")
-rand{T<:ZModN}(::Type{T}, dims...) = rand!(T, Array(T, dims))
+
+function rand{T<:ZModN}(::Type{T}, dims...)
+    if length(dims) > 0
+        rand!(T, Array(T, dims))
+    else
+        rand_(T)
+    end
+end
 
 if VERSION >= v"0.4-"
-    rand{N,I<:Integer}(rng::AbstractRNG, T::Type{ZRing{N,I}}) = 
-    ZR(N, rand(rng, one(I):convert(I,N)))
-    rand{N,I<:Integer}(rng::AbstractRNG, T::Type{ZField{N,I}}) = 
-    ZF(N, rand(rng, one(I):convert(I,N)))
+    rand{J<:Integer,N,I<:Integer}(rng::AbstractRNG, T::Type{ZRing{J,N,I}}) = ZR(N, J, rand(rng, one(I):convert(I,N)), I)
+    rand{J<:Integer,N,I<:Integer}(rng::AbstractRNG, T::Type{ZField{J,N,I}}) = ZF(N, J, rand(rng, one(I):convert(I,N)), I)
     function rand!{T<:ZModN}(rng::AbstractRNG, ::Type{T}, a::AbstractArray)
         for i in 1:length(a)
             a[i] = rand(rng, T)
@@ -152,20 +168,20 @@ if VERSION >= v"0.4-"
 end
 
 # equal N from types, so we don't need to test explicitly
-=={N,I}(a::ZModN{N,I}, b::ZModN{N,I}) = a.i == b.i
-<={N,I}(a::ZModN{N,I}, b::ZModN{N,I}) = a.i <= b.i
-<{N,I}(a::ZModN{N,I}, b::ZModN{N,I}) = a.i < b.i
+=={T,N,I}(a::ZModN{T,N,I}, b::ZModN{T,N,I}) = a.i == b.i
+<={T,N,I}(a::ZModN{T,N,I}, b::ZModN{T,N,I}) = a.i <= b.i
+<{T,N,I}(a::ZModN{T,N,I}, b::ZModN{T,N,I}) = a.i < b.i
 
-real{N,I}(a::ZModN{N,I}) = a
-abs{N,I}(a::ZModN{N,I}) = a
+real{T,N,I}(a::ZModN{T,N,I}) = a
+abs{T,N,I}(a::ZModN{T,N,I}) = a
 
-liftz(c, n, i, f, a) = c(convert(i, mod(f(a.i), n)))
-liftz(c, n, i, f, a, b) = c(convert(i, mod(f(a.i, b.i), n)))
+liftz(c, i, f, a) = c(convert(i, mod(f(a.i), modulus(a))))
+liftz(c, i, f, a, b) = c(convert(i, mod(f(a.i, b.i), modulus(a))))
 for Z in (ZRing, ZField)    
-    @eval -{N,I}(a::$Z{N,I}) = liftz($Z{N,I}, N, I, -, a)
-    @eval ^{N,I}(a::$Z{N,I}, p::Int) = liftz($Z{N,I}, N, I, x -> powermod(x, p, N), a)
+    @eval -{J,N,I}(a::$Z{J,N,I}) = liftz($Z{J,N,I}, I, -, a)
+    @eval ^{J,N,I}(a::$Z{J,N,I}, p::Int) = liftz($Z{J,N,I}, I, x -> powermod(x, p, modulus(a)), a)
     for op in (:+, :-, :*, :&, :|, :$)
-        @eval $op{N,I}(a::$Z{N,I}, b::$Z{N,I}) = liftz($Z{N,I}, N, I, $op, a, b)
+        @eval $op{J,N,I}(a::$Z{J,N,I}, b::$Z{J,N,I}) = liftz($Z{J,N,I}, I, $op, a, b)
     end
 end
 
@@ -182,9 +198,20 @@ function extended_euclidean{I<:Integer}(a::I, n::I)
     t < 0 ? t + n : t
 end
 
-inv{N,I}(a::ZRing{N,I}) = ZRing{N,I}(extended_euclidean(N, convert(I, N)))
-inv{N,I}(a::ZField{N,I}) = a^(N-2)
-/{N,I}(a::ZModN{N,I}, b::ZModN{N,I}) = a * inv(b)
+inv{J,N,I}(a::ZRing{J,N,I}) = ZRing{J,N,I}(extended_euclidean(a.i, convert(I, modulus(a))))
+inv{J,N,I}(a::ZField{J,N,I}) = a^(modulus(a)-2)
+
+function /{T,N,I}(a::ZModN{T,N,I}, b::ZModN{T,N,I})
+    if a == b
+        one(a)
+    elseif a == zero(a)
+        a
+    elseif b == zero(b)
+        error("division by zero: $a/$b")
+    else
+        a * inv(b)
+    end
+end
 
 # macros to rewrite literals mod n 
 # (these rewrite ALL ints, including args to literal ZF(...), etc)
@@ -210,29 +237,105 @@ end
 
 
 
-# --- supertype for all polynomial representations
+# --- supertype for all integer polynomial representations
 
-abstract Poly <: Residue
+# I is the type that [] returns when indexing a coefficient
+abstract Poly{I<:Integer} <: Residue
 
 endof(a::Poly) = length(a)
 start(::Poly) = 1
 done(a::Poly, i) = i > length(a)
 next(a::Poly, i) = (a[i], i+1)
 
+length{P<:Poly}(a::P) = error("define a length() for type $P")
+getindex{P<:Poly}(a::P, i) = error("define a getindex() for type $P")
+
+# delegate modulus to the coefficient type
+
+modulus{T}(::Poly{T}) = modulus(T)
+modulus{T}(::Type{Poly{T}}) = modulus(T)   # is this useful?
+
+# generic comparison
+
+function cmp{P<:Poly}(a::P, b::P, eq)
+    if length(a) < length(b)
+        true
+    elseif length(a) > length(b)
+        false
+    else
+        for i in length(a):-1:1
+            if a[i] < b[i]
+                return true
+            elseif a[i] > b[i]
+                return false
+            end
+        end
+        eq
+    end
+end
+<={P<:Poly}(a::P, b::P) = cmp(a, b, true)
+<{P<:Poly}(a::P, b::P) = cmp(a, b, false)
+
+# exotic polynomials may require a suitable show_coeff and/or show_mod to
+# display well
+
+show_coeff(io::IO, i::Integer) = show(io, i)
+show_coeff{T,N,I<:Integer}(io::IO, z::ZModN{T,N,I}) = show(io, convert(I, z))
+
+show_mod{I<:Integer}(io::IO, p::Poly{I}) = nothing
+
+function print_no_mod{I<:Integer}(io::IO, p::Poly{I})
+    n = length(p)
+    if n <= 0
+        show_coeff(io, zero(I))
+    else
+        for j = n:-1:1
+            pj = p[j]
+            if pj != zero(I)
+                if j == n
+                    pj < 0 && print(io, "-")
+                else
+                    pj < 0 ? print(io," - ") : print(io," + ")
+                end
+                magpj = abs(pj)
+                if j == 1 || magpj != one(I)
+                    show_coeff(io, magpj)
+                end
+                if j > 1
+                    print(io, :x)
+                    if j > 2
+                        print(io, '^', j-1)
+                    end
+                end
+            end
+        end
+    end
+end
+
+function print{I<:Integer}(io::IO, p::Poly{I})
+    print_no_mod(io, p)
+    show_mod(io, p)
+end
+
+function show{I<:Integer}(io::IO, p::Poly{I})
+    print(io, "ZP($I")
+    for i in length(p):-1:1
+        print(io, ",")
+        show_coeff(io, p[i])
+    end
+    print(io, ")")
+end
+
 
 # --- arbitrary (but "integer") coeff polynomials
 
-
-# started with Polynomial.jl, but that has various issues; this is
-# less general (and faster and uses less memory - see PTests.jl).
-
-# IMPORTANT - arrays may be shared between polynomials, so don't mutate
-# contents unless you have a new instance whose contents you know to be
-# owned.
-
-immutable ZPoly{I<:Integer} <: Poly
+immutable ZPoly{I<:Integer} <: Poly{I}
     a::Vector{I}
     ZPoly(a) = length(a) == 0 || a[end] != zero(I) ? new(a) : error("zero leading coeff")
+end
+
+immutable FZP{I<:Integer}
+    a::Tuple
 end
 
 # constructors
@@ -256,19 +359,10 @@ ZP(t::Type, coeffs...) = ZP(reverse(t[map(c -> convert(t, c), coeffs)...]))
 ZP(c::Function, coeffs...) = ZP(reverse([map(c, coeffs)...]))
 ZP{I<:Integer}(coeffs::I...) = ZP(reverse([coeffs...]))
 
-# needed below to squeeze polynomial into type
-# not via convert as we don't know type (length of tuple).  
-# TODO - this may cause efficiency issues (since return type varies)?
-encode_factor{I<:Integer}(p::ZPoly{I}) = tuple(map(a -> convert(Int, a), p.a)...)
-decode_factor{I<:Integer}(::Type{ZPoly{I}}, t::Tuple) = ZPoly{I}([map(i -> convert(I, i), t)...])
-
 # nice syntax for creating polynomials.
 # use x=X(ZF(2)) or x=X(GF2) or x=X(ZField{2,Int}) and then x^2+3 etc
 X(c::Function) = ZP([c(0), c(1)])
 X{I<:Integer}(::Type{I}) = ZP([zero(I), one(I)])
-
-modulus{T}(::ZPoly{T}) = modulus(T)
-modulus{T}(::Type{ZPoly{T}}) = modulus(T)
 
 # this promotion to write x^2+1 etc
 convert{I<:Integer}(::Type{ZPoly{I}}, p::ZPoly{I}) = p
@@ -280,53 +374,7 @@ getindex(a::ZPoly, i) = getindex(a.a, i)
 setindex!{T,I<:Integer}(a::ZPoly{T}, v::I, i) = setindex!(a.a, convert(T, v), i)
 length(a::ZPoly) = length(a.a)
 
-show_int{N,I<:Integer}(io::IO, z::ZModN{N,I}) = show(io, convert(I, z))
-show_int{I<:Integer}(io::IO, i::I) = show(io, convert(I, i))
-print_mod{Z<:ZModN}(io::IO, p::ZPoly{Z}) = print(io, " mod $(modulus(Z))")
-print_mod{I<:Integer}(io::IO, p::ZPoly{I}) = nothing
-
-function print_no_mod{I<:Integer}(io::IO, p::ZPoly{I})
-    n = length(p)
-    if n <= 0
-        show_int(io, zero(I))
-    else
-        for j = n:-1:1
-            pj = p[j]
-            if pj != zero(I)
-                if j == n
-                    pj < 0 && print(io, "-")
-                else
-                    pj < 0 ? print(io," - ") : print(io," + ")
-                end
-                magpj = abs(pj)
-                if j == 1 || magpj != one(I)
-                    show_int(io, magpj)
-                end
-                if j > 1
-                    print(io, :x)
-                    if j > 2
-                        print(io, '^', j-1)
-                    end
-                end
-            end
-        end
-    end
-end
-
-function print{I<:Integer}(io::IO, p::ZPoly{I})
-    print_no_mod(io, p)
-    print_mod(io, p)
-end
-
-function show{I<:Integer}(io::IO, p::ZPoly{I})
-    print(io, "ZP($I")
-    for i in length(p):-1:1
-        print(io, ",")
-        show_int(io, p[i])
-    end
-    print(io, ")")
-end
-
+show_mod{Z<:ZModN}(io::IO, p::ZPoly{Z}) = print(io, " mod $(modulus(Z))")
 showcompact(io::IO, p::ZPoly) = showcompact(io, p.a)
 
 # number methods
@@ -337,26 +385,15 @@ one{T}(::Type{ZPoly{T}}) = ZPoly{T}([one(T)])
 one{Z<:ZPoly}(z::Z) = one(Z)
 
 =={T}(a::ZPoly{T}, b::ZPoly{T}) = a.a == b.a
-function cmp{T}(a::ZPoly{T}, b::ZPoly{T}, eq)
-    if length(a) < length(b)
-        true
-    elseif length(a) > length(b)
-        false
-    else
-        for i in length(a):-1:1
-            if a[i] < b[i]
-                return true
-            elseif a[i] > b[i]
-                return false
-            end
-        end
-        eq
-    end
-end
-<={T}(a::ZPoly{T}, b::ZPoly{T}) = cmp(a, b, true)
-<{T}(a::ZPoly{T}, b::ZPoly{T}) = cmp(a, b, false)
-
 -{T}(a::ZPoly{T}) = ZPoly{T}(-a.a)
+
+function trailing_zeros{T}(p::ZPoly{T})
+    n = 1
+    while n < length(p) && p[n] == zero(T)
+        n += 1
+    end
+    n - 1
+end
 
 # a and b same length; discard leading zeroes
 function _truncate{T}(f, a::Vector{T}, b::Vector{T})
@@ -507,13 +544,14 @@ for op in (:&, :|, :$)
 end
 <<{T}(a::ZPoly{T}, n::Int) = ZPoly{T}(vcat(zeros(T, n), a.a))
 >>>{T}(a::ZPoly{T}, n::Int) = ZPoly{T}(a.a[n+1:end])
+>>{T}(a::ZPoly{T}, n::Int) = a >>> n
 
 
 
 # --- GF(2) polynomials encoded as bits
 
-
-immutable GF2Poly{U<:Unsigned} <: Poly
+# the type of Poly{} is the type returned by []
+immutable GF2Poly{U<:Unsigned} <: Poly{ZField{Int,2,Int}}
     i::U
 end
 
@@ -523,9 +561,6 @@ GF2P{S<:Signed}(p::S) = GF2P(unsigned(p))
 GF2X{U<:Unsigned}(::Type{U}) = GF2Poly{U}(one(U) + one(U))
 GF2X() = GF2X(Uint)
 
-encode_factor{U<:Unsigned}(p::GF2Poly{U}) = tuple(convert(Int, p.i))
-decode_factor{U<:Unsigned}(::Type{GF2Poly{U}}, t::Tuple) = GF2Poly{U}(convert(U, t[1]))
-
 modulus(::Type{GF2Poly}) = 2
 modulus(::GF2Poly) = 2
 
@@ -534,7 +569,7 @@ promote_rule{I<:Unsigned, U<:Unsigned}(::Type{GF2Poly{U}}, ::Type{I}) = GF2Poly{
 promote_rule{U<:Unsigned}(::Type{GF2Poly{U}}, ::Type{Int}) = GF2Poly{U}
 
 # promote to ZPoly mainly for equality tests (convert below)
-promote_rule{U<:Unsigned,I<:Integer}(::Type{GF2Poly{U}}, ::Type{ZPoly{ZField{2,I}}}) = ZPoly{ZField{2,I}}
+promote_rule{U<:Unsigned,J<:Integer,I<:Integer}(::Type{GF2Poly{U}}, ::Type{ZPoly{ZField{J,2,I}}}) = ZPoly{ZField{J,2,I}}
 
 # no setindex! as immutable
 getindex{U<:Unsigned}(a::GF2Poly{U}, i) = a.i & (one(U) << (i-1)) == 0 ? GF2(0) : GF2(1)
@@ -544,8 +579,8 @@ length{U<:Unsigned}(a::GF2Poly{U}) = 8 * sizeof(U) - leading_zeros(a.i)
 # but we probably need the interop anyway, and who cares if printing
 # is efficient?
 
-convert{I<:Integer, U<:Unsigned}(::Type{ZPoly{ZField{2,I}}}, p::GF2Poly{U}) = _convert(ZPoly{ZField{2,I}}, p)
-convert{I<:Integer, U<:Unsigned}(::Type{GF2Poly{U}}, p::ZPoly{ZField{2,I}}) = _convert(GF2Poly{U}, p)
+convert{J<:Integer, I<:Integer, U<:Unsigned}(::Type{ZPoly{ZField{J,2,I}}}, p::GF2Poly{U}) = _convert(ZPoly{ZField{J,2,I}}, p)
+convert{J<:Integer, I<:Integer, U<:Unsigned}(::Type{GF2Poly{U}}, p::ZPoly{ZField{J,2,I}}) = _convert(GF2Poly{U}, p)
 convert{U<:Unsigned}(::Type{GF2Poly{U}}, p::GF2Poly{U}) = p
 
 function _convert{T, F}(::Type{T}, from::F)
@@ -560,8 +595,9 @@ function _convert{T, F}(::Type{T}, from::F)
     result
 end
 
-print{U<:Unsigned}(io::IO, p::GF2Poly{U}) = print(io, convert(ZPoly{ZField{2,Int}}, p))
-print_no_mod{U<:Unsigned}(io::IO, p::GF2Poly{U}) = print_no_mod(io, convert(ZPoly{ZField{2,Int}}, p))
+#print{U<:Unsigned}(io::IO, p::GF2Poly{U}) = print(io, convert(ZPoly{ZField{2,Int}}, p))
+#print_no_mod{U<:Unsigned}(io::IO, p::GF2Poly{U}) = print_no_mod(io, convert(ZPoly{ZField{2,Int}}, p))
+show_mod{U<:Unsigned}(io::IO, p::GF2Poly{U}) = print(io, " mod 2")
 show{U<:Unsigned}(io::IO, p::GF2Poly{U}) = print(io, "GF2Poly{$U}($(p.i))")
 
 bits(p::GF2Poly) = bits(p.i)
@@ -580,6 +616,7 @@ for op in (:(==), :<=, :<)
 end
 <<{U<:Unsigned}(a::GF2Poly{U}, n::Int) = GF2Poly(a.i << n)
 >>>{U<:Unsigned}(a::GF2Poly{U}, n::Int) = GF2Poly(a.i >>> n)
+>>{U<:Unsigned}(a::GF2Poly{U}, n::Int) = a >>> n
 leading_zeros{U<:Unsigned}(a::GF2Poly{U}) = leading_zeros(a.i)
 
 function *{U<:Unsigned}(a::GF2Poly{U}, b::GF2Poly{U})
@@ -643,3 +680,10 @@ degree{U<:Unsigned}(p::GF2Poly{U}) = 8*sizeof(U) - leading_zeros(p)
 
 
 end
+
+
+
+#ZField{ATP ZP(IntModN.ZField{2,Int64},1,0,0,0,1,1,0,1,1),
+#       IntModN.ZPoly{IntModN.ZField{2,Int64}}}(x^6 + x^5 + x^3 + x^2 + x + 1 mod 2) 
+#ZField{ATP ZP(IntModN.ZField{2,Int64},1,0,0,0,1,1,0,1,1),
+#       IntModN.ZPoly{IntModN.ZField{2,Int64}}}(x^6 + x^4 + x + 1 mod 2)
